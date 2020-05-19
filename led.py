110196c049ff89b20174e747994ad73e03de87bd
Plamen Delchev <plamen.delchev@gmail.com>
2020-05-19 13:16:39 +0100

    Added LEDs dict for better structure

--- led.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-
-class Led(GPIO):
-    def __init__(self, channel, brightness, frequency):
-        self.channel = channel
-        self.brightness = brightness
-        self.frequency = frequency
-
-        self.PWM(self.channel, self.frequency)
--- strip.py
+++ strip.py
@@ -2,39 +2,43 @@ import RPi.GPIO as GPIO
 
 class Strip():
     def __init__(self, channels):
+        # GPIO Initial Settings
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(channels, GPIO.OUT)
 
-        self._red = GPIO.PWM(channels[0], 100)
-        self._green = GPIO.PWM(channels[1], 100)
-        self._blue = GPIO.PWM(channels[2], 100)
-        self._white = GPIO.PWM(channels[3], 100)
+        # Initialize a dictionary with all LED colors (keys) and their GPIO.PWM objects (values)
+        self.leds = {'red': None, 'green': None, 'blue': None, 'white': None}
 
+        for channel, color in zip(channels, self.leds): 
+            led = GPIO.PWM(channel, 100)
+            self.leds[color] = led
+
+    # Turn on all LEDs
     def on(self, brightness=50):
-        self._red.start(brightness)
-        self._green.start(brightness)
-        self._blue.start(brightness)
-        self._white.start(brightness)
+        for led in self.leds.values():
+            led.start(brightness)
 
+    # Turn off all LEDs
     def off(self):
-        self._red.stop()
-        self._green.stop()
-        self._blue.stop()
-        self._white.stop()
+        for led in self.leds.values():
+            led.stop()
 
+    # Set a specific color to the whole strip == set a specific color to a specific LED
     def color(self, r, g, b, w):
-        self._red.ChangeDutyCycle(r)
-        self._green.ChangeDutyCycle(g)
-        self._blue.ChangeDutyCycle(b)
-        self._white.ChangeDutyCycle(w)
+        self.leds['red'].ChangeDutyCycle(r)
+        self.leds['green'].ChangeDutyCycle(g)
+        self.leds['blue'].ChangeDutyCycle(b)
+        self.leds['white'].ChangeDutyCycle(w)
 
+    # Set a specific PWM frequency to the whole strip == change the frequency of each individual LED
     def frequency(self, r, g, b, w):
-        self._red.ChangeFrequency(r)
-        self._green.ChangeFrequency(g)
-        self._blue.ChangeFrequency(b)
-        self._white.ChangeFrequency(w)
+        self.leds['red'].ChangeFrequency(r)
+        self.leds['green'].ChangeFrequency(g)
+        self.leds['blue'].ChangeFrequency(b)
+        self.leds['white'].ChangeFrequency(w)
 
+    # Turn off all leds and set all GPIO pins to LOW
     def poweroff(self):
         self.off()
         GPIO.cleanup()
