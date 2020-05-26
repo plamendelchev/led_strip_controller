dd19ff18f36e2e9b8677a6d9d2117394b59d0a6a
Plamen Delchev <plamen.delchev@gmail.com>
2020-05-26 23:36:57 +0100

    Minor improvements for readability on bin/run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-SOURCE='/home/pi/projects/led_strip_controller/docker'
-TARGET='/usr/src/app'
-IMAGE='led_controller'
-VER='0.8'
+set -o errexit
 
-docker run -p 5000:5000 --env FLASK_ENV=development --mount type=bind,source="${SOURCE}",target="${TARGET}" --device='/dev/gpiomem':'/dev/mem' "${IMAGE}:${VER}"
+docker run \
+	--expose 5000:5000 \
+	--env FLASK_ENV=development \ 
+	--mount type=bind,source='/home/pi/projects/led_strip_controller/docker',target='/usr/src/app' \
+	--device='/dev/gpiomem':'/dev/mem' \ 
+	'led_controller:0.8'
